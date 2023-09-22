ce10eca75bed966da98292081ce808dd4af5c0af
Pingumaniac <63883314+Pingumaniac@users.noreply.github.com>
2023-09-22 09:15:34 -0500

    Add files via upload

---
+++ CHAPTER 4: THE REPRESENTATION AND INTERPRETATION OF PROGRAMS/ex4_7.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+#|
+The result of (VALOF e) = value of the variable whose name is computed as the value of e.
+
+Allows to import names into internal contexts
+-> not a desirable feature as it is very complex to use.
+|#
